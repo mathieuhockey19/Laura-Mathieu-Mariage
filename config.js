5f1ec1a7d1c39a1738926e012f1b5802bcf1d0a8
mathieuhockey19 <mathieuhockey19@gmail.com>
2026-08-12 08:41:27 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // URL Google Apps Script pour enregistrer les RSVP.
 // On la renseignera ensemble lors de la mise en ligne.
-const GOOGLE_SCRIPT_URL = "";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyCvYap8OcwFnMY1g72n2AC1emPg05QORZVYs5uFPLWLZ4yrOOzgow_hbwXpk7Ja22gDA/exec";
 
 // Hébergements : on complétera cette liste après les réponses des établissements.
 const HOTELS = [
